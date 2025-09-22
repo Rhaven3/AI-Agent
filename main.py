6e07214b6b09d01cfd5a71074745448695918b14
Rhaven <f.nom.utilisateur@gmail.com>
2025-09-22 16:35:36 +0200

    prompt with arg

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import os
+from sys import argv, exit
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
 def main():
-    print("Hello from ai-agent!")
+    if len(argv) < 2:
+        print("Usage: python main.py '<your prompt here>'")
+        exit(1)
+
+    user_prompt = argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=messages
+    )
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}\nResponse: {response.text}")
 
 
 if __name__ == "__main__":
